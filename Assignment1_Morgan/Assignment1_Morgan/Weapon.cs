e4043267ce297ddd01c1d5ee0810d654c187d6b3
Morgan <146908044+MorganChinTang@users.noreply.github.com>
2026-01-13 00:08:05 -0800

    Assignment 1
    A C# Console Application that parses a csv file.
    Output.csv is in ...\GitHub\VGP232_Assignments_Morgan\Assignment1_Morgan\Assignment1_Morgan\bin\Debug\net8.0\output.csv

---
+++ Assignment1_Morgan/Assignment1_Morgan/Weapon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Weapon
+    {
+        // Name,Type,Rarity,BaseAttack
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Rarity { get; set; }
+        public int BaseAttack { get; set; }
+        
+        /// <summary>
+        /// The Comparator function to check for name
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        public static int CompareByName(Weapon left, Weapon right)
+        {
+            return left.Name.CompareTo(right.Name);
+        }
+
+        // TODO: add sort for each property:
+        /// <summary>
+        /// The Comparator function to check for type
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        // CompareByType
+        public static int CompareByType(Weapon left, Weapon right)
+        {
+            return left.Type.CompareTo(right.Type);
+        }
+
+        /// <summary>
+        /// The Comparator function to check for rarity
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        // CompareByRarity
+        public static int CompareByRarity(Weapon left, Weapon right)
+        {
+            return left.Rarity.CompareTo(right.Rarity);
+        }
+
+        /// <summary>
+        /// The Comparator function to check for base attack
+        /// </summary>
+        /// <param name="left">Left side Weapon</param>
+        /// <param name="right">Right side Weapon</param>
+        /// <returns> -1 (or any other negative value) for "less than", 0 for "equals", or 1 (or any other positive value) for "greater than"</returns>
+        // CompareByBaseAttack
+        public static int CompareByBaseAttack(Weapon left, Weapon right)
+        {
+            return left.BaseAttack.CompareTo(right.BaseAttack);
+        }
+
+        /// <summary>
+        /// The Weapon string with all the properties
+        /// </summary>
+        /// <returns>The Weapon formated string</returns>
+        public override string ToString()
+        {
+            // TODO: construct a comma seperated value string
+            // Name,Type,Rarity,BaseAttack
+            return string.Format("{0},{1},{2},{3}", Name, Type, Rarity, BaseAttack);
+        }
+    }
+}
